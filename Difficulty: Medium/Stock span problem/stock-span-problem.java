7e6ebd24a39b42983183db32df627d2da3614952
Roshani Prakash Choudhari <chuodharirshani@gmail.com>
2026-04-22 22:08:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<Integer> calculateSpan(int[] arr) {
+        // code here
+        ArrayList<Integer> list=new ArrayList<>();
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            while(!st.isEmpty() && arr[st.peek()]<=arr[i]){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                list.add(i+1);
+            }else{
+                list.add(i-st.peek());
+            }
+            st.push(i);
+        }
+        return list;
+    }
+}
