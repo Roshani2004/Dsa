7b103dde4b18ea747a4efee4d543aefe441e650b
Roshani Prakash Choudhari <chuodharirshani@gmail.com>
2026-04-22 18:54:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Count Ways)/coin-change-count-ways.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int count(int coins[], int sum) {
+        // code here.
+        int dp[]=new int[sum+1];
+        dp[0]=1;
+        for(int i=0;i<coins.length;i++){
+            for(int j=coins[i];j<=sum;j++){
+                dp[j]=dp[j]+dp[j-coins[i]];
+            }
+        }
+        return dp[sum];
+    }
+}
